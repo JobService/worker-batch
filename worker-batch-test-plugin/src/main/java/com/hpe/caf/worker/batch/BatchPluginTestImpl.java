99588a731696ebb4168585709c6ee231a2b5d6d6
Michael McAlynn <michael.mcalynn@hpe.com>
2016-03-07 13:17:07 +0000

    CAF-603 - Testing asset ID Plugin.

--- worker-batch-test-plugin/src/main/java/com/hpe/caf/worker/batch/BatchPluginTestImpl.java
+++ worker-batch-test-plugin/src/main/java/com/hpe/caf/worker/batch/BatchPluginTestImpl.java
@@ -9,9 +9,6 @@ import java.util.Map;
  * A very simple implementation of the batch worker plugin. Will split a comma separated list into subtasks.
  */
 public class BatchPluginTestImpl implements BatchWorkerPlugin {
-
-    public static String identifier = "BATCH_WORKER_TEST_IMPL";
-
     @Override
     public void processBatch(BatchWorkerServices batchWorkerServices, String batchDefinition, String taskMessageType, Map<String, String> taskMessageParams) {
         List<String> items = Arrays.asList(batchDefinition.split("\\s*,\\s*"));
@@ -19,7 +16,4 @@ public class BatchPluginTestImpl implements BatchWorkerPlugin {
             batchWorkerServices.registerItemSubtask("Test task", 1, subItem);
         }
     }
-
-
-
 }
