7ee6d1ebb85702dfe60eaf101c00ed310f90b4f9
Aaron O'Loughlin <aaron.oloughlin@hpe.com>
2016-07-20 14:31:48 +0100

    CAF-913: Updated BatchWorkerAcceptanceIT to use TestNG rather then JUnit

--- worker-batch-container-build/src/test/java/BatchWorkerAcceptanceIT.java
+++ worker-batch-container-build/src/test/java/BatchWorkerAcceptanceIT.java
@@ -1,14 +1,40 @@
 import com.hpe.caf.worker.batch.BatchTestControllerProvider;
-import com.hpe.caf.worker.testing.execution.TestRunner;
-import org.junit.Test;
+import com.hpe.caf.worker.testing.TestControllerSingle;
+import com.hpe.caf.worker.testing.TestItem;
+import com.hpe.caf.worker.testing.execution.TestControllerProvider;
+import com.hpe.caf.worker.testing.execution.TestRunnerSingle;
+import org.testng.annotations.*;
+
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  * Created by gibsodom on 26/02/2016.
  */
 public class BatchWorkerAcceptanceIT {
-    @Test
-    public void testWorker() throws Exception {
+    TestControllerProvider testControllerProvider;
+    TestControllerSingle controller;
 
-        TestRunner.runTests(new BatchTestControllerProvider());
+    @BeforeClass
+    public void setUp() throws Exception {
+        testControllerProvider = new BatchTestControllerProvider();
+        controller =  TestRunnerSingle.getTestController(testControllerProvider, false);
+        controller.initialise();
+    }
+
+    @AfterClass
+    public void tearDown() throws Exception{
+        controller.close();
+    }
+
+    @DataProvider(name = "MainTest")
+    public Iterator<Object[]> createData() throws Exception {
+        Set<Object[]> s = TestRunnerSingle.setUpTest(testControllerProvider);
+        return s.iterator();
+    }
+
+    @Test(dataProvider = "MainTest")
+    public void testWorker(TestItem testItem) throws Exception {
+        controller.runTests(testItem);
     }
 }
