fdc26b9c09ef70cdea430f33b66de41d745d53a5
dmilligan <dmilligan@opentext.com>
2026-07-15 11:08:30 +0100

    removed ws

--- worker-batch/src/test/java/BatchPluginTestImpl.java
+++ worker-batch/src/test/java/BatchPluginTestImpl.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 import com.github.jobservice.workers.batch.BatchWorkerPlugin;
 import com.github.jobservice.workers.batch.BatchWorkerServices;
 
